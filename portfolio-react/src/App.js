2012b50fba2248d5496e87a947e2faba86069c12
Taylor Leong <tjl2125@alum.barnard.edu>
2021-06-21 22:27:31 -0400

    fixed Header and Footer

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,9 +1,9 @@
 import React from "react"; 
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
-import Projects from "./components/Projects/Projects";
-import Resume from "./components/Resume/Resume"; 
+// import Home from "./components/Home/Home";
+// import Projects from "./components/Projects/Projects";
+// import Resume from "./components/Resume/Resume"; 
 import Footer from "./components/Footer/Footer"; 
 import "./App.css"; 
 
@@ -12,11 +12,11 @@ function App() {
         <Router>
             <div className = "App">
                 <Header />
-                <Switch>
+                {/* <Switch>
                     <Route path="/" exact component={() => <Home />} />
                     <Route path="/projects" exact component={() => <Projects />} />
                     <Route path="/Resume" exact component={() => <Resume />} />
-                </Switch>
+                </Switch> */}
                 <Footer />
             </div>
         </Router>
--- portfolio-react/src/components/Footer/Footer.js
+++ portfolio-react/src/components/Footer/Footer.js
@@ -3,13 +3,20 @@ import React from "react";
 function Footer() {
     return(
         <div className = "footer">
-            <footer className = "py-sm-5 d-inline-block bg-primary">
-            <div className = "contact-container">
+            <footer className = "py-3 bg-dark fixed-bottom">
+            <div className = "contact-container text-white text-center">
 
-                <section className = "phone">
-
+                <section className = "email">
+                    <p>Email: tjl2125@alum.barnard.edu</p>
                 </section>
 
+                <section className = "github-profile">
+                    <p>Github: https://github.com/tjl2125</p>
+                </section>
+
+                <section className = "linkedin">
+                    <p>LinkedIn: https://www.linkedin.com/in/taylor-leong-1bbb13179/</p>
+                </section>
 
             </div>
             </footer>
