c7bf456907d2132467cbb95000ba9076cb5832ae
Taylor Leong <tjl2125@alum.barnard.edu>
2021-06-22 18:29:15 -0400

    starting Resume

--- portfolio-react/src/components/Resume/Resume.js
+++ portfolio-react/src/components/Resume/Resume.js
@@ -1,1 +1,17 @@
 import React from "react";  
+
+function Resume () {
+    return (
+        <div className= "resume">
+            <div className= "container">
+
+
+
+
+
+
+
+            </div>
+        </div>
+    )
+}
