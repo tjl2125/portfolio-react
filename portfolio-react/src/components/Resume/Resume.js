067385985104367d3a7533b393af4a163990a0a8
Taylor Leong <tjl2125@alum.barnard.edu>
2021-06-22 22:32:28 -0400

    adding to ReadMe and resume

--- portfolio-react/src/components/Resume/Resume.js
+++ portfolio-react/src/components/Resume/Resume.js
@@ -4,13 +4,11 @@ function Resume () {
     return (
         <div className= "resume">
             <div className= "container">
-
-
-
-
-
-
-
+                <div className = "row justify-content-md-center">
+                    <div className = "col-6">
+                        
+                    </div>
+                </div>
             </div>
         </div>
     )
